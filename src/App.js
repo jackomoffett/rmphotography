e21ee0485c8469d647c933260ddd385e89188501
jackomoffett <jack_moffett@icloud.com>
2021-05-24 21:50:15 +0100

    Starter stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Rachel Moffett Photography</p>
       </header>
+      <div className="App-content">
+        <p className="description">
+          The only thing I wanted when I was small was a pony. Thankfully,
+          growing up in the countryside near Taunton, Somerset, meant I was able
+          to help others look after and ride theirs. I have kept horses, off and
+          on, for the rest of my adult life, along with dogs. My parents were
+          keen photographers and so a passion for photography also emerged. I
+          have found a real love of attempting to capture the essence of dogs
+          and horses in photographs, with and without their owners. I hope you
+          enjoy viewing my images as much as I enjoyed making them and if you
+          are interested in having some photos taken, please contact me. Rache
+        </p>
+      </div>
     </div>
   );
 }
