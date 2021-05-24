d07be1a3d0d267536110493ff34c832e6e28ef85
jackomoffett <jack_moffett@icloud.com>
2021-05-25 00:01:11 +0100

    Different direction

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Rachel Moffett Photography</p>
+        <p className="App-header-logo">Rachel Moffett Photography</p>
+        <ul className="App-header-links">
+          <li>
+            <a href="https://www.facebook.com/RachelMoffettphotography/?ref=page_internal">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/RachelMoffettphotography/?ref=page_internal">
+              Instagram
+            </a>
+          </li>
+        </ul>
       </header>
-      <div className="App-content">
-        <p className="description">
-          The only thing I wanted when I was small was a pony. Thankfully,
-          growing up in the countryside near Taunton, Somerset, meant I was able
-          to help others look after and ride theirs. I have kept horses, off and
-          on, for the rest of my adult life, along with dogs. My parents were
-          keen photographers and so a passion for photography also emerged. I
-          have found a real love of attempting to capture the essence of dogs
-          and horses in photographs, with and without their owners. I hope you
-          enjoy viewing my images as much as I enjoyed making them and if you
-          are interested in having some photos taken, please contact me. Rache
-        </p>
-      </div>
+      <div className="App-content"></div>
     </div>
   );
 }
