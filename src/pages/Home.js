d2bd631288b047f2f55af2b7a22ac31afe9a4c5d
Jack Moffett <jack.moffett@ukproahclijmof01.local>
2021-06-15 23:10:47 +0100

    Updated styles etc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="Home">
       <div className="block">
-        <img src="horse.jpeg" alt="dog" height="450" width="450" />
+        <img src="horse2.jpeg" alt="dog" height="450" width="450" />
         <img src="flower.jpeg" alt="dog" height="450" width="450" />
         <img src="dog.jpeg" alt="dog" height="450" width="450" />
       </div>
