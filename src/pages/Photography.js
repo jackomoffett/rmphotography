637773343680d519201a1b06db2696fb65ca2e57
jackomoffett <jack_moffett@icloud.com>
2021-07-28 22:14:59 +0100

    Pricing update

--- src/pages/Photography.js
+++ src/pages/Photography.js
@@ -4,7 +4,7 @@ function Photography() {
       <div className="Photography-Wrapper">
         <p>Photography</p>
         <p>
-          My fees are £60 for up to a 1 hour session within a 30 mile roundtrip
+          My fees are £65 for up to a 1 hour session within a 30 mile roundtrip
           of Hambridge, Somerset. (25p per mile extra, if further than this.)
           This can include horses, dogs, family members and black background
           images if the situation allows. This session could also be used to
@@ -41,7 +41,7 @@ function Photography() {
         </p>
         <p>
           Preview files should be with you within 7 days of being taken and
-          ordered prints with you within 3 weeks of order date. The £60
+          ordered prints with you within 3 weeks of order date. The £65
           photography fee is payable before the session.
         </p>
         <p>If you would like any more information please contact me.</p>
