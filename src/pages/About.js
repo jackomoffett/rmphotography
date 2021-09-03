38abab899a8ee10584b166d20fab6ae15cda0822
jackomoffett <jack_moffett@icloud.com>
2021-09-03 20:30:12 +0100

    Updated print and about me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,21 +6,27 @@ function About() {
       <div className="About-text">
         <p>About me</p>
         <p>
-          Photography and horses have been lifelong partners to me. Born and
-          raised in the Somerset countryside, around many different animals, I
-          was desperate for a pony. I first rode when I was 7 and I have owned
-          horses on and off since then. Dogs came along just a little later but
-          have an equally large place in my heart. Their honesty and devotion a
-          constant surprise.
+          Photography has been a life long journey for me. Born and raised in
+          the Somerset countryside, I grew up roaming the surrounding area. I
+          was always accompanied by many different animals, all captured (images
+          treasured) with my small Kodak camera.
         </p>
         <p>
-          My photography become more serious with my first DSLR 15 years ago.
-          During the interim years I have experimented with many differing types
-          of photography, from abstract to fast motion captures. All still
-          interest me, but I find I am drawn to photographing animals,
-          constantly striving to capture images of honesty, beauty, truth and
-          connection.
+          I made the move into DSLRs, when my family grew. I wanted better
+          quality memories which I was in control of, without the frozen
+          expressions we were getting from school/set-up photography sessions.
         </p>
+        <p>
+          In the last couple of years, I have been engaged in many differing
+          types of photography (examples can be found in my 'gallery' section.)
+          I find photography very immersive and am constantly striving to move
+          forward.
+        </p>
+        <p>
+          Please email me if you would like to discuss having some images taken.
+        </p>
+        <p>Prices are contained in the Fees section of the website.</p>
+        <p>Thank you, Rachel.</p>
       </div>
       <div className="About-img">
         <img src={rmImage} alt="Rachel Moffett" />
